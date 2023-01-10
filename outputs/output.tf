60bcd7f5cd2e9238255169858bf7be07ea54e1c5
abbadisaneep <sandeep.ajr123@gmail.com>
2023-01-10 10:18:41 +0530

    output file is used to print output on the screen

--- outputs/output.tf
+++ outputs/output.tf
@@ -1,7 +1,11 @@
+output "test" {
+  value = "hello"
+}
+
 data "aws_ami" "example" {
   most_recent = true
   name_regex = "Centos-8-Devops-Practice"
-  owners = ["370086682536"]
+  owners = ["973714476881"]
 
 }
 
