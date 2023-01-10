f4cfd3b38fd3a5e1a833b542092d1b638a43fc10
abbadisaneep <sandeep.ajr123@gmail.com>
2023-01-10 09:41:27 +0530

    output.tf file is used to display lines on the terminal or screen

--- outputs/output.tf
+++ outputs/output.tf
@@ -1,9 +1,10 @@
 data "aws_ami" "example" {
   most_recent = true
   name_regex = "Centos-8-Devops-Practice"
+  owners = "370086682536"
 }
 
 output "AMI_ID" {
   value = data.aws_ami.example.id
 }
-kfgfgbvb
+
