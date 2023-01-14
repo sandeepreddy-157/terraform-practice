20be3b14accfae61c6ae01f1121c90996745a556
sandeepabbadi <sandeepabbadi.vts@gmail.com>
2023-01-14 12:00:14 +0530

    The file contains the necessary information for Terraform to create and configure the resources

--- resources/main.tf
+++ resources/main.tf
@@ -1,8 +1,25 @@
-resource "aws_instance" "test" {
-  ami = "ami-0a017d8ceb274537d"
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_security_group" "example" {
+  name        = "example"
+  description = "Example security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0a017d8ceb274537d"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.example.id]
 }
 
 output "public_ip" {
-  value = aws_instance.test.public_ip
+  value = aws_instance.example.public_ip
 }
