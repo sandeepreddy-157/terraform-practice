79cc76fbe3e5de27a11bee840eb4211b5a078947
sandeepabbadi <sandeepabbadi.vts@gmail.com>
2023-01-18 21:38:38 +0530

    The file contains the necessary information for Terraform to create and configure the resources

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,5 +1,5 @@
 #resource "aws_instance" "test" {
-#  ami = "ami-0a017d8ceb274537d"
+#  ami = "vpc-04c5c896c4d9202bd"
 #  instance_type = "t3.micro"
 #  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
