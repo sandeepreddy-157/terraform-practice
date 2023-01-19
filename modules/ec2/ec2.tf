b6b60c611f6968b97fbf6ef914868218c4b75965
sandeepabbadi <sandeepabbadi.vts@gmail.com>
2023-01-19 12:06:40 +0530

    The file contains the necessary information for Terraform to create and configure the resources

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "test" {
-  ami = "ami-0a017d8ceb274537d"
+  ami = var.ami
   instance_type = "t3.micro"
   vpc_security_group_ids = [var.sgid]
 
 }
 
 variable "sgid"{}
+variable "ami"{}
--- modules/main.tf
+++ modules/main.tf
@@ -5,4 +5,11 @@ module "sg"{
 module "ec2" {
   source = "./ec2"
   sgid = module.sg.sgid
+  ami = "ami-0a017d8ceb274537d"
+}
+
+module "ec21"{
+  source = "./ec2"
+  sgid = module.sg.sgid
+  ami = "ami-0bb6af715826253bf"
 }
