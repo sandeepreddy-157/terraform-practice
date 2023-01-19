f49cb9d2a12ad7a9d13f9c6a95881002cd10bce4
sandeepabbadi <sandeepabbadi.vts@gmail.com>
2023-01-19 19:49:45 +0530

    count" loop in Terraform is used to create a specific number of resources.

--- modules/loops/count/main.tf
+++ modules/loops/count/main.tf
@@ -2,4 +2,11 @@ resource "aws_instance" "myec2"{
   count = 2
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t2.micro"
+  tags = {
+    Name = "instance-${count.index}"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.myec2.*.public_ip
 }
