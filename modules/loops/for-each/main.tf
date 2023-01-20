6a854ea63406cf60dfc9c72988ede07fa225e9a7
sandeepabbadi <sandeepabbadi.vts@gmail.com>
2023-01-20 23:30:43 +0530

    The for_each meta-argument accepts a map or a set of strings, and creates an instance for each item in that map or set

--- modules/loops/for-each/main.tf
+++ modules/loops/for-each/main.tf
@@ -6,7 +6,6 @@ variable "instances"{
     }
     two = {
       ami ="ami-0bb6af715826253bf"
-      instance_type= "t2.micro"
     }
   }
 }
@@ -14,7 +13,7 @@ variable "instances"{
 resource "aws_instance" "test3"{
   for_each = var.instances
   ami = each.value["ami"]
-  instance_type = each.value["instance_type"]
+  instance_type = lookup(each.value, "instance_type", null) ==null ? "t2.micro" : each.value["instance_type"]
   tags = {
     Name = "instance-${each.key}"
   }
